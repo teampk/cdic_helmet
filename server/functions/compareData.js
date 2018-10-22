d9df978c762b13ad602274cdb352883c443b6dde
nuggy875 <jinugpaeng@gmail.com>
2018-10-23 00:32:12 +0900

    remove complete

--- server/functions/compareData.js
+++ server/functions/compareData.js
@@ -5,14 +5,18 @@ function compareImageData(id, x, y, w, h, c, r){
 
     new Promise(function(resolve,reject){
 		var compareCondition = { $and:[{"labelClass": c}, 
-		{"labelRatio": {$gt:r-1, $lt:r+1}}
+		{"labelRatio": {$gt:r-0.1, $lt:r+0.1}}
 		]};
 		
 		imageData.find(compareCondition)
 		.then(function(foundDatas){
 			console.log('found data:', foundDatas);
-			// --- delete ---
-			imageData.deleteMany(compareCondition)
+			if (foundDatas[0]){
+				console.log(foundDatas[0].dataId);
+			}
+		
+			// --- delete all old data---
+			imageData.deleteMany({})
 			.then(function(removedDatas){
 				console.log('removed');
 				// --- register new data ---
@@ -26,12 +30,6 @@ function compareImageData(id, x, y, w, h, c, r){
 		.catch(function(err){
 			console.log(err);
 		});
-
-		console.log('if not');
-
-		
-		
-
     });
     
 }
